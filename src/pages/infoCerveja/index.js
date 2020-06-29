d8655cdfbb1169a08e14356e79ab338876c011be
caiquegl <arq.caique@hotmail.com>
2020-06-29 17:20:09 -0300

    falta finalizar a div de comentarios e avaliação na pg info

--- src/pages/infoCerveja/index.js
+++ src/pages/infoCerveja/index.js
@@ -15,6 +15,20 @@ import Aus from '../../assets/aus.svg';
 import CopoPale from '../../assets/copoPale.png';
 import Ml from '../../assets/ml.png';
 import Valor from '../../assets/valor.svg';
+import Cervejaria from '../../assets/cervejaria.png';
+import Cor2 from '../../assets/cor-2.jpg';
+import Temperatura from '../../assets/temperatura.png';
+import An3 from '../../assets/n3.jpg';
+import Aparencia from '../../assets/aparencia.jpg';
+import Aroma from '../../assets/aroma.jpg';
+import Percent from '../../assets/percent.svg';
+import CarneVermelha from '../../assets/carne-vermelha.jpg';
+import CarneAves from '../../assets/carne-aves.jpg';
+
+
+
+
+
 
 
 
@@ -58,50 +72,125 @@ export default function InfoCerveja(){
                 </div>
             </div>
     {/* fim do header */}
+        <div className="container-info">
+            <div className="pai-info">
 
-        <div className="pai-info">
-
-            <div className="foto">
-                <Zoom
-                img={Cooper}
-                zoomScale={3}
-                width={500}
-                height={500}
-                />
-                <div className="info-zoom">
-                    <img src={Search} alt=""/>
-                    <p>Passe o mouse para ver os detalhes. Imagem meramente ilustrativa.</p>
+                <div className="foto">
+                    <Zoom
+                    img={Cooper}
+                    zoomScale={3}
+                    width={500}
+                    height={500}
+                    />
+                    <div className="info-zoom">
+                        <img src={Search} alt=""/>
+                        <p>Passe o mouse para ver os detalhes. Imagem meramente ilustrativa.</p>
+                    </div>
                 </div>
+
+                <div className="info-descri">
+                    <div className="descri">
+                        <h2> Coopers Original Pale Ale 375ml</h2>
+                        <p>
+                            A Coopers Original Pale Ale é uma cerveja de notas frutadas e florais, com o equilíbrio perfeito entre o frutado e o amargor. ideal para quem está começando no estilo. 
+                        </p>
+                    </div>
+                    <div className="dados">
+                            <img src={Aus} />
+                            <p>
+                                Australia
+                            </p>
+                            <img src={CopoPale}/>
+                            <p>
+                                English Pale Ale
+                            </p>
+                            <img src={Ml}/>
+                            <p>
+                                375ml
+                            </p>
+                            <img src={Valor}/>
+                            <p>
+                                +/- R$ 20,00
+                            </p>
+                            
+                    </div>
+                </div>                        
             </div>
 
-            <div className="info-descri">
-                <div className="descri">
-                    <h2> Coopers Original Pale Ale 375ml</h2>
-                    <p>
-                        A Coopers Original Pale Ale é uma cerveja de notas frutadas e florais, com o equilíbrio perfeito entre o frutado e o amargor. ideal para quem está começando no estilo. 
-                    </p>
-                </div>
-                <div className="dados">
-                        <img src={Aus} />
-                        <p>
-                            Australia
-                        </p>
-                        <img src={CopoPale}/>
-                        <p>
-                            English Pale Ale
-                        </p>
-                        <img src={Ml}/>
-                        <p>
-                            375ml
-                        </p>
-                        <img src={Valor}/>
-                        <p>
-                            +/- R$ 20,00
-                        </p>
-                        
-                </div>
+            <div className="mais-descri">
+                <h2>Saiba mais sobre este produto</h2>
+                <p>A Coopers Original Pale Ale é uma cerveja de notas frutadas e florais, com o equilíbrio perfeito entre o frutado e o amargor ideal para quem está começando no estilo. </p>
 
-                    
+                <h3>cerveja coopers original pale ale</h3>
+                <div className="all-descri">
+                    <div className="left">
+                        <section>
+                            <img src={Aus}/>
+                            <h2>Origem:</h2>
+                            <p>Adelaide - Australia</p>
+                        </section>
+                        <section>
+                            <img src={Cervejaria}/>
+                            <h2>Cervejaria:</h2>
+                            <p>Coopers</p>
+                        </section>
+                        <section>
+                            <img src={CopoPale} className="img-copo"/>
+                            <h2>Estilo:</h2>
+                            <p>English Pale Ale</p>
+                        </section>
+                        <section>
+                            <img src={Ml} className="img-copo"/>
+                            <h2>Volume:</h2>
+                            <p>375ml</p>
+                        </section>
+                        <section>
+                            <img src={Cor2} className="cor-dourada"/>
+                            <h2>Coloração:</h2>
+                            <p>Dourada</p>
+                        </section>
+                        <section>
+                            <img src={Temperatura} className="temp"/>
+                            <h2>Temperatura ideal:</h2>
+                            <p>12º a 14º</p>
+                        </section>
+                    </div>
+                    <div className="right">
+                        <section>
+                            <img src={An3} className="amargor"/>
+                            <h2>Amargor:</h2>
+                            <p>Moderado</p>
+                        </section>
+                        <section>
+                            <img src={Aparencia} />
+                            <h2>Aparência:</h2>
+                            <p>Levemente turva</p>
+                        </section>
+                        <section>
+                            <img src={Aroma} />
+                            <h2>Aroma:</h2>
+                            <p>Pão, mel e notas cítricas do lúpulo.</p>
+                        </section>
+                        <section>
+                            <img src={Percent} />
+                            <h2>Teor alcoólico:</h2>
+                            <p>4,5%</p>
+                        </section>
+                        <div className="harmonizacao">
+                            <h2>Sugestão de harmonização:</h2>
+                            <div className="itens">
+                                <div>
+                                    <img src={CarneVermelha}/>
+                                    <p>Carne vermelha</p>
+                                </div>
+                                <div>
+                                    <img src={CarneAves}/>
+                                    <p>Aves</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <Footer />
